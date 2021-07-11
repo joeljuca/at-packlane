ea9cb4361d6052e1ef76f422d036b1b45df47717
Joel Jucá <joelwallis@gmail.com>
2021-07-11 15:41:28 -0300

    test: introduce command `mix text.watch`

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Packbox.MixProject do
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mix_test_watch, "~> 1.0", only: [:test]}
     ]
   end
 
